8e8a81c88572525abad12fe136eebffa24477ad2
Klemen Pogacnik <k.pogacnik@iskratel.si>
2020-12-23 21:17:44 +0100

    workin on vm creation and deletion

--- plugins/filter/vr_filters.py
+++ plugins/filter/vr_filters.py
@@ -2,38 +2,37 @@
 # hello_world collection's filter plugins
 #########################################
 
-# networks_to_list filter
-# Prepare a list of networks in a form suitable for vm creation
-def networks_to_list(networks_in):
-    # print(networks_in)
-    networks_out = []
+# netint_to_list filter
+# Prepare a list of network interfaces in a form suitable for vm creation
+def netint_to_list(netint_in):
+    # print(netint_in)
+    netint_out = []
     ii = 3
-    for net_name, net_data in networks_in.items():
+    for netint_name, netint_data in netint_in.items():
         net_info = {}
-        net_info['name'] = net_name
-        net_info['type'] = net_data['type']
-        if (net_data['type'] == 'bridge'):
-            net_info['net'] = net_data['ext_bridge']
+        net_info['name'] = netint_name
+        if ('ext_bridge' in netint_data.keys()):
+            net_info['type'] = 'bridge'
+            net_info['network'] = netint_data['ext_bridge']
         else:
-            net_info['net'] = net_name
-        if ('cidr' in net_data.keys()):
-            net_info['address'] = net_data['cidr']
-        if ('interface_name' in net_data.keys()):
-            net_info['interface_name'] = net_data['interface_name']
+            net_info['type'] = 'network'
+            net_info['network'] = netint_data['network']
+        if (('boot' in netint_data.keys()) and (netint_data['boot'] == 'yes')):
+            net_info['boot_order'] = '2'
+        if ('address_cidr' in netint_data.keys()):
+            net_info['address_cidr'] = netint_data['address_cidr']
+        if ('interface_name' in netint_data.keys()):
+            net_info['interface_name'] = netint_data['interface_name']
         else:
             net_info['interface_name'] = 'ens' + str(ii)
             ii += 1
-        if (('boot' in net_data.keys()) and (net_data['boot'] == 'yes')):
-            net_info['boot_order'] = '2'
-        if ('gateway' in net_data.keys()):
-            net_info['gateway'] = net_data['gateway']
-        if ('domain_name' in net_data.keys()):
-            net_info['domain_name'] = net_data['domain_name']
-        if ('nameservers' in net_data.keys()):
-            net_info['nameservers'] = net_data['nameservers']
-        networks_out.append(net_info)
-    # print(networks_out)
-    return networks_out
+        if ('gateway' in netint_data.keys()):
+            net_info['gateway'] = netint_data['gateway']
+        if ('nameservers' in netint_data.keys()):
+            net_info['nameservers'] = netint_data['nameservers']
+        netint_out.append(net_info)
+    # print(netint_out)
+    return netint_out
 
 
 # volume_to_list filter
@@ -62,6 +61,6 @@ def volumes_to_list(volumes_in):
 class FilterModule(object):
     def filters(self):
         return {
-                'networks_to_list': networks_to_list,
+                'netint_to_list': netint_to_list,
                 'volumes_to_list': volumes_to_list
         }
